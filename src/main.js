01975aa3b0adda5e5dce40cfc81adbb0bc8706b7
Denys Polishchuk <polischyk0000@gmail.com>
2024-03-02 15:06:30 +0200

    add test onclick

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// const mobileNavigation = document.getElementById('header-menu');
+
+// mobileNavigation.addEventListener('click', function () {
+//   document.getElementById('view-mobile-navigation').classList.add('is-open');
+//   console.log(document.getElementById('view-mobile-navigation'));
+// });
+
+function logText() {
+  console.log('text');
+}
