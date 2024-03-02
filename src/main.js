9634420288eece5b5254a2b45c0b0fa7fb43c9b8
Denys Polishchuk <polischyk0000@gmail.com>
2024-03-03 00:40:52 +0200

    create exit menu

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-// const mobileNavigation = document.getElementById('header-menu');
+const headerOpenMenuSvg = document.getElementById('header-menu-svg-btn');
+const mobileNavigation = document.getElementById('view-mobile-navigation');
+const mobileNavigationCloseMenuSvg = document.getElementById('btn-close');
 
-// mobileNavigation.addEventListener('click', function () {
-//   document.getElementById('view-mobile-navigation').classList.add('is-open');
-//   console.log(document.getElementById('view-mobile-navigation'));
-// });
+headerOpenMenuSvg.addEventListener('click', function () {
+  mobileNavigation.classList.remove('is-close');
+  mobileNavigation.classList.add('is-open');
+});
 
-function logText() {
-  console.log('text');
-}
+mobileNavigationCloseMenuSvg.addEventListener('click', function () {
+  mobileNavigation.classList.remove('is-open');
+  mobileNavigation.classList.add('is-close');
+});
